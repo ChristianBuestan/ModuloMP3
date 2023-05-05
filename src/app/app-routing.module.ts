645e8c8dc6ea68be4df99bd943f5c8fcfc6227ef
Your Name~ <you@example.com>
2023-05-05 16:39:05 -0500

    mayo5-16-38

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { CatalogoComponent } from './pages/catalogo/catalogo.component';
+import { BusquedaComponent } from './pages/busqueda/busqueda.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: "", component:  BusquedaComponent},
+  {path: "pages/catalogo", component: CatalogoComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CatalogoComponent } from './pages/catalogo/catalogo.component';
+import { BusquedaComponent } from './pages/busqueda/busqueda.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CatalogoComponent,
+    BusquedaComponent
   ],
   imports: [
     BrowserModule,
